394a1198cf1b23f06aad909cb79fb26f367b6095
stephane <stephanesome@gmail.com>
2023-07-11 17:49:10 -0400

    Final

---
+++ src/main/kotlin/seg3x02/rabbitchat/MessageProducer.kt
@@ -0,0 +1,27 @@
+package seg3x02.rabbitchat
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+import kotlin.system.exitProcess
+
+@Component
+class MessageProducer(val template: RabbitTemplate): CommandLineRunner {
+    @Value("\${chat.alias}")
+    val USER: String=""
+
+    override fun run(vararg args: String?) {
+        println("**** You are connected to Simple Chat ****")
+        println("**** Enter #BYE to terminate          ****")
+        println("******************************************")
+        println()
+        var msg = readLine()
+        while (msg != null && msg != "#BYE") {
+            template.convertAndSend("chatfan", "", "$USER: $msg")
+            msg = readLine()
+        }
+        println("Shutting Down ...")
+        exitProcess(0)
+    }
+}
